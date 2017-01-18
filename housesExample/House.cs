5c4598388e48193a29ef76383957f191f86f247b
Samuel Saunders <sam.saunders116@yahoo.com>
2017-01-18 10:33:55 -0500

    change

--- housesExample/House.cs
+++ housesExample/House.cs
@@ -6,21 +6,40 @@ using System.Threading.Tasks;
 
 namespace housesExample
 {
-    class House
+    class House//class
     {
-        private string color;
-        private int rooms;
-        private int size;
-        private int doors;
+        private string color;//field
+        private int rooms;//field
+        private int size;//field
+        private int doors;//field
 
-        public void Paint(string color)
+        public House(string color, int rooms, int size, int doors)//constructor// class and constructor have to have the same name
+        {
+            this.color = color;//varibles
+            this.rooms = rooms;//varibles
+            this.size = size;//varibles
+            this.doors = doors;//varibles
+
+        }
+
+        
+        public void Paint(string color)//method 
+            
         {
             this.color = color;
         }
-        public string CurrentColor()
+        public string CurrentColor()//
         {
             return color;
         }
+        public void DisplayHouseInfo()
+        {
+            Console.Write("House Color: " + color);
+            Console.Write(" ,  Rooms: " + rooms);
+            Console.Write(" , Size is: " + size + " sq/ft ");
+            Console.Write(" , with: " + doors + " "+ "doors ");
+        }
+
        
         
     }
--- housesExample/Program.cs
+++ housesExample/Program.cs
@@ -10,6 +10,24 @@ namespace housesExample
     {
         static void Main(string[] args)
         {
+         //   House house1 = new House();                 //how to call from class
+
+          //  house1.Paint("Green");
+           // Console.WriteLine(house1.CurrentColor());
+
+          //  house1.Paint("White");
+          //  Console.WriteLine(house1.CurrentColor());
+
+         //   House house2 = new House();
+         //   house2.Paint("Blue");
+
+         //   Console.WriteLine("house one is: " + house1.CurrentColor());
+         //   Console.WriteLine("house two is: " + house2.CurrentColor());
+
+            House house1 = new House("Green", 4, 1000, 3);
+            house1.DisplayHouseInfo();
+
+
         }
     }
 }
